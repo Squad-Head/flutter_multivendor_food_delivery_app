89a1ea25a8d2a2c2add359a9468d29d7557ee8ff
ambanikousik <rikousik@gmail.com>
2022-04-18 15:12:08 +0600

    added firestore

--- multivendor_food_delivery_customer/lib/main.dart
+++ multivendor_food_delivery_customer/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:multivendor_food_delivery_customer/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
